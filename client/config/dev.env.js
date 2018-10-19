153de29ccb429abf2bebb532726883556690ae3f
肖洒 <xiaos@songcw.com>
2018-10-19 15:40:13 +0800

    端口改成30000

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HTTPBASEURL: '"http://localhost:20000"',
+  HTTPBASEURL: '"http://localhost:30000"',
   HTTPOPENAPIURL: '"http://192.168.31.247:10001"'
 });
