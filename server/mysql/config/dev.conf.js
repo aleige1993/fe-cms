4550b14c9c3291517f1f817f1ae12dc901588187
黄磊 <huangl@songcw.com>
2018-10-19 13:10:03 +0800

    111

--- server/mysql/config/dev.conf.js
+++ server/mysql/config/dev.conf.js
@@ -1,17 +1,17 @@
 
 module.exports = {
-  CONNECTION: {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '123456',
-    PORT: 3306
-  },
   // CONNECTION: {
-  //   HOST: '192.168.31.247',
+  //   HOST: 'localhost',
   //   USER: 'root',
-  //   PASSWORD: 'songcw',
+  //   PASSWORD: '123456',
   //   PORT: 3306
   // },
+  CONNECTION: {
+    HOST: '192.168.31.247',
+    USER: 'root',
+    PASSWORD: 'songcw',
+    PORT: 3306
+  },
   DATABASE: {
     SC_CMS: 'sc_cms_dev'
   }
